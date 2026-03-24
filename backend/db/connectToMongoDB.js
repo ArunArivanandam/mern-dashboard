f03a28d97519e2f00f6f6d2eecc082a703caa798
ArunArivanandam <arunarivanandam@gmail.com>
2026-03-24 17:25:55 +0530

    connect to mongodb

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+require("dotenv").config();
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("Connected to mongodb");
+  } catch (error) {
+    console.log("Error connecting to mongodb", error.message);
+  }
+};
+
+module.exports = connectToMongoDB;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const app = require("./app");
+const http = require("http");
+const connectToMongoDB = require("./db/connectToMongoDB");
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  connectToMongoDB();
+  console.log("Connected to server on port 3000");
+});
