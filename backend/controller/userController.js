6dfaf08ec50537f4002f6b17058da0c5b0614823
ArunArivanandam <arunarivanandam@gmail.com>
2026-03-24 19:09:18 +0530

    user saved to mongodb

---
+++ backend/controller/userController.js
@@ -0,0 +1,17 @@
+const User = require("../models/userModel");
+
+exports.getAllUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (error) {
+    res.status(400);
+    console.log("Error at creating user", error.message);
+  }
+};
--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
 require("dotenv").config();
 
 const connectToMongoDB = async () => {
