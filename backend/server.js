c458d51b6854a14eb0e2c57c2c0285d8ad633113
ArunArivanandam <arunarivanandam@gmail.com>
2026-03-25 12:07:30 +0530

    server file improved

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const app = require("./app");
 const connectToMongoDB = require("./db/connectToMongoDB");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: "./.env" });
+dotenv.config();
+
 app.listen(3000, () => {
   connectToMongoDB();
   console.log("Connected to server on port 3000");
