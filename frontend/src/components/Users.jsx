0f4f192a95fb150bb700937e11907a870b78e2dc
ArunArivanandam <arunarivanandam@gmail.com>
2026-03-29 12:24:15 +0530

    user details displayed in table using tailwindcss

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -23,10 +23,34 @@ function Users() {
   return (
     <div>
       <h2>Users List</h2>
+      <table className="w-full border border-gray-200 mt-5">
+        <thead className="bg-gray-100">
+          <tr>
+            <th className="p-3 text-left">Name</th>
+            <th className="p-3 text-left">Email</th>
+            <th className="p-3 text-left">Age</th>
+            <th className="p-3 text-left">Role</th>
+            <th className="p-3 text-left">Status</th>
+          </tr>
+        </thead>
 
-      {users.map((user) => (
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id} className="border-t hover:bg-gray-50">
+              <td className="p-3">{user.userName}</td>
+              <td className="p-3">{user.email}</td>
+              <td className="p-3">{user.age}</td>
+              <td className="p-3">{user.role}</td>
+              <td className="p-3">
+                {user.isActive ? "🟢 Active" : "🔴 Inactive"}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* {users.map((user) => (
         <p key={user._id}>{user.userName}</p>
-      ))}
+      ))} */}
     </div>
   );
 }
